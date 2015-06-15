69977cc52f3513b9d0639625a09957c3e725ab37
micobg <micobg@gmail.com>
2015-06-15 21:16:25 +0300

    words split test

--- Indexer/Indexer.java
+++ Indexer/Indexer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Index all words in given text.
@@ -12,7 +14,7 @@ public class Indexer {
 
     public static void main(String[] args) {
         String fileToBeIndexed = args[0];
-        Path pathToFile = FileSystems.getDefault().getPath("files", fileToBeIndexed);
+        Path pathToFile = FileSystems.getDefault().getPath("src", "files", fileToBeIndexed);
 
         try {
             readFile(pathToFile);
@@ -27,6 +29,16 @@ public class Indexer {
     private static void readFile(Path pathToFile) throws IndexerException {
         try {
             Files.lines(pathToFile).forEach((String line) -> {
+                System.out.println(line);
+
+                Pattern  pattern = Pattern.compile("\\p{L}+");
+                Matcher matcher = pattern.matcher(line);
+                while(matcher.find()) {
+                    String a = matcher.group();
+                    System.out.println(a);
+                }
+
+
                 // extract words
 
                 // for each word
